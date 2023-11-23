a38bdfec4619dfa478e0df23c2b26f9509e9d380
KangNuri <nuris0421@gmail.com>
2023-11-23 17:15:14 +0900

    Initial commit

---
+++ todo/src/main/java/com/example/todo/todoapi/dto/request/TodoModifyRequestDTO.java
@@ -0,0 +1,19 @@
+package com.example.todo.todoapi.dto.request;
+
+import lombok.*;
+
+import javax.validation.constraints.NotBlank;
+
+@Setter
+@Getter
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TodoModifyRequestDTO {
+
+    @NotBlank
+    private String id;
+    private boolean done;
+}
---
+++ todo/src/main/java/com/example/todo/todoapi/entity/Todo.java
@@ -0,0 +1,32 @@
+package com.example.todo.todoapi.entity;
+
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
+@Setter @Getter
+@ToString
+@EqualsAndHashCode(of = "todoId")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name="tbl_todo")
+public class Todo {
+
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name="system-uuid", strategy = "uuid")
+    private String todoId;
+
+    @Column(nullable = false, length = 30)
+    private String title; //할 일
+
+    private boolean done; // 할 일 완료 여부
+
+    @CreationTimestamp // 자동으로 값 드가도록
+    private LocalDateTime createDate; // 등록 시간
+}
